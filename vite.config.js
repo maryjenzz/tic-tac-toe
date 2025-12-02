fffb9f2112e389b4b5139c7dc4c844e1da3cdc82
maryjenzz <mylenamari34@gmail.com>
2025-12-02 16:15:13 -0300

    fix: corrected absolute base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = '/tic-tac-toe/';
+// O nome do seu repositório é 'tic-tac-toe'
+// REVERTA PARA O CAMINHO RELATIVO COMPLETO DO SUBDIRETÓRIO
+const repoName = '/tic-tac-toe/'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // A base deve ser o nome do repositório, com barras laterais.
   base: repoName, 
 })
