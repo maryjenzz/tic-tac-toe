12944134f9468b8a6bbd28720997d740415a8772
maryjenzz <mylenamari34@gmail.com>
2025-12-02 15:54:19 -0300

    feat: config github pages and deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = 'https://github.com/maryjenzz/tic-tac-toe'; 
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: repoName, 
 })
