919423dad2b630a8dda90a04e58f7cbc545954a0
maryjenzz <mylenamari34@gmail.com>
2026-01-12 15:54:12 -0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// O nome do seu repositório é 'tic-tac-toe'
-// REVERTA PARA O CAMINHO RELATIVO COMPLETO DO SUBDIRETÓRIO
 const repoName = '/tic-tac-toe/'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // A base deve ser o nome do repositório, com barras laterais.
-  base: repoName, 
+  base: 'tic-tac-toe', 
 })
